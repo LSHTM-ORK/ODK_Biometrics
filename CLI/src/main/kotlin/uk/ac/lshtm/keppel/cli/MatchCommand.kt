72b18fc453cfbde991fa904627e0d3290e3cca25
Callum Stott <callum@seadowg.com>
2020-03-13 15:00:19 +0000

    Add help cases for -p flag

--- CLI/src/main/kotlin/uk/ac/lshtm/keppel/cli/MatchCommand.kt
+++ CLI/src/main/kotlin/uk/ac/lshtm/keppel/cli/MatchCommand.kt
@@ -7,14 +7,24 @@ class MatchCommand(
         private val threshold: Double) {
 
     fun execute(args: List<String>, logger: Logger): Boolean {
-        if (args.size < 2) {
+        if (args.isEmpty()) {
             logger.log("Usage: match [TEMPLATE_FILE_1] [TEMPLATE_FILE_2]")
             return false
         }
 
         val (templateOne, templateTwo) = if (args.first() == "-p") {
+            if (args.size < 3) {
+                logger.log("Usage: match -p [TEMPLATE_1] [TEMPLATE_2]")
+                return false
+            }
+
             Pair(args[1].toByteArray(), args[2].toByteArray())
         } else {
+            if (args.size < 2) {
+                logger.log("Usage: match [TEMPLATE_FILE_1] [TEMPLATE_FILE_2]")
+                return false
+            }
+
             Pair(readAndTrim(File(args[0])), readAndTrim(File(args[1])))
         }
 
--- CLI/src/test/kotlin/uk/ac/lshtm/keppel/cli/MatchCommandTest.kt
+++ CLI/src/test/kotlin/uk/ac/lshtm/keppel/cli/MatchCommandTest.kt
@@ -14,46 +14,43 @@ class MatchCommandTest {
 
     private val fileOne = createTempFile().apply { writeText("finger-1\n") }
     private val fileTwo = createTempFile().apply { writeText("  finger-2") }
+    private val logger = FakeLogger()
 
     @Test
     fun whenMatchIsLessThanThreshold_returnsFalseAndLogsScore() {
-        val logger = mock<Logger>()
         val matcher = mock<Matcher> {
             on { match(any(), any()) } doReturn 5.0
         }
 
         val matchCommand = MatchCommand(matcher, 10.0)
         assertThat(matchCommand.execute(listOf(fileOne.absolutePath, fileTwo.absolutePath), logger), equalTo(false))
-        verify(logger).log("Not a match. Score: 5.0")
+        assertThat(logger.lines, equalTo(listOf("Not a match. Score: 5.0")))
     }
 
     @Test
     fun whenEqualToThreshold_returnsTrueAndLogsScore() {
-        val logger = mock<Logger>()
         val matcher = mock<Matcher> {
             on { match(any(), any()) } doReturn 10.0
         }
 
         val matchCommand = MatchCommand(matcher, 10.0)
         assertThat(matchCommand.execute(listOf(fileOne.absolutePath, fileTwo.absolutePath), logger), equalTo(true))
-        verify(logger).log("Match! Score: 10.0")
+        assertThat(logger.lines, equalTo(listOf("Match! Score: 10.0")))
     }
 
     @Test
     fun whenGreaterThanThreshold_returnsTrueAndLogsScore() {
-        val logger = mock<Logger>()
         val matcher = mock<Matcher> {
             on { match(any(), any()) } doReturn 11.0
         }
 
         val matchCommand = MatchCommand(matcher, 10.0)
         assertThat(matchCommand.execute(listOf(fileOne.absolutePath, fileTwo.absolutePath), logger), equalTo(true))
-        verify(logger).log("Match! Score: 11.0")
+        assertThat(logger.lines, equalTo(listOf("Match! Score: 11.0")))
     }
 
     @Test
     fun passesTrimmedFileContentToMatcher() {
-        val logger = mock<Logger>()
         val matcher = mock<Matcher> {
             on { match(any(), any()) } doReturn 5.0
         }
@@ -65,33 +62,59 @@ class MatchCommandTest {
 
     @Test
     fun withNoArguments_returnsFalseAndLogsHelp() {
-        val logger = mock<Logger>()
         val matcher = mock<Matcher>()
         val matchCommand = MatchCommand(matcher, 10.0)
 
         assertThat(matchCommand.execute(listOf(), logger), equalTo(false))
-        verify(logger).log("Usage: match [TEMPLATE_FILE_1] [TEMPLATE_FILE_2]")
+        assertThat(logger.lines, equalTo(listOf("Usage: match [TEMPLATE_FILE_1] [TEMPLATE_FILE_2]")))
     }
 
     @Test
     fun withOneArgument_returnsFalseAndLogsHelp() {
-        val logger = mock<Logger>()
         val matcher = mock<Matcher>()
         val matchCommand = MatchCommand(matcher, 10.0)
 
         assertThat(matchCommand.execute(listOf(fileOne.absolutePath), logger), equalTo(false))
-        verify(logger).log("Usage: match [TEMPLATE_FILE_1] [TEMPLATE_FILE_2]")
+        assertThat(logger.lines, equalTo(listOf("Usage: match [TEMPLATE_FILE_1] [TEMPLATE_FILE_2]")))
     }
 
     @Test
     fun withPFlag_whenMatchIsLessThanThreshold_returnsFalseAndLogsScore() {
-        val logger = mock<Logger>()
         val matcher = mock<Matcher> {
             on { match(any(), any()) } doReturn 5.0
         }
 
         val matchCommand = MatchCommand(matcher, 10.0)
         assertThat(matchCommand.execute(listOf("-p", Hex.encodeHexString("finger-1".toByteArray()), Hex.encodeHexString("finger-1".toByteArray())), logger), equalTo(false))
-        verify(logger).log("Not a match. Score: 5.0")
+        assertThat(logger.lines, equalTo(listOf("Not a match. Score: 5.0")))
+    }
+
+    @Test
+    fun withPFlag_withNoArguments_returnsFalseAndLogsHelp() {
+        val matcher = mock<Matcher>()
+        val matchCommand = MatchCommand(matcher, 10.0)
+
+        assertThat(matchCommand.execute(listOf("-p"), logger), equalTo(false))
+        assertThat(logger.lines, equalTo(listOf("Usage: match -p [TEMPLATE_1] [TEMPLATE_2]")))
+    }
+
+    @Test
+    fun withPFlag_withOneArgument_returnsFalseAndLogsHelp() {
+        val matcher = mock<Matcher>()
+        val matchCommand = MatchCommand(matcher, 10.0)
+
+        assertThat(matchCommand.execute(listOf("-p", Hex.encodeHexString("finger-1".toByteArray())), logger), equalTo(false))
+        assertThat(logger.lines, equalTo(listOf("Usage: match -p [TEMPLATE_1] [TEMPLATE_2]")))
+    }
+}
+
+private class FakeLogger : Logger {
+    private val _lines: ArrayList<String> = arrayListOf()
+
+    val lines: List<String>
+        get() = _lines
+
+    override fun log(text: String) {
+        _lines.add(text)
     }
 }
