656495fab8ec1d6e6e07d9fa09377f2978b16ea0
Callum Stott <callum@seadowg.com>
2024-07-15 16:42:22 +0100

    Show toast when scanner is connected

--- Android/app/src/main/java/uk/ac/lshtm/keppel/android/settings/UsbDeviceAttachedActivity.kt
+++ Android/app/src/main/java/uk/ac/lshtm/keppel/android/settings/UsbDeviceAttachedActivity.kt
@@ -2,10 +2,15 @@ package uk.ac.lshtm.keppel.android.settings
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import uk.ac.lshtm.keppel.android.R
 
 class UsbDeviceAttachedActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Toast.makeText(this, R.string.scanner_connected, Toast.LENGTH_LONG).show()
+        finish()
     }
 }
