f3b24871ce9bc112a8d52820cda58279dd9572ff
Callum Stott <callum@seadowg.com>
2024-07-15 16:35:00 +0100

    Show different specific Activity for USB attached event

---
+++ Android/app/src/main/java/uk/ac/lshtm/keppel/android/settings/UsbDeviceAttachedActivity.kt
@@ -0,0 +1,11 @@
+package uk.ac.lshtm.keppel.android.settings
+
+import android.app.Activity
+import android.os.Bundle
+
+class UsbDeviceAttachedActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
