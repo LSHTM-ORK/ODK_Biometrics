41a31bea9e0bf6cece4e7b61c5d5e0ca414d573a
Callum Stott <callum@seadowg.com>
2024-08-07 14:05:51 +0100

    Increase timeout to 30 seconds

--- Android/biomini/src/main/java/uk/ac/lshtm/keppel/biomini/BioMiniScanner.kt
+++ Android/biomini/src/main/java/uk/ac/lshtm/keppel/biomini/BioMiniScanner.kt
@@ -266,30 +266,35 @@ class BioMiniScanner(private val context: Context) : Scanner {
         }
     }
 
-    private fun setTemplateType() {
-        val currentDevice = mCurrentDevice
-        if (currentDevice != null) {
-            val tmp_type = IBioMiniDevice.TemplateType.ISO19794_2.value()
-            currentDevice.setParameter(
-                IBioMiniDevice.Parameter(
-                    IBioMiniDevice.ParameterType.TEMPLATE_TYPE,
-                    tmp_type.toLong(),
-                ),
+    private fun setParameters(iBioMiniDevice: IBioMiniDevice) {
+        iBioMiniDevice.setParameter(
+            IBioMiniDevice.Parameter(
+                IBioMiniDevice.ParameterType.TEMPLATE_TYPE,
+                IBioMiniDevice.TemplateType.ISO19794_2.value().toLong(),
+            ),
+        )
+
+        iBioMiniDevice.setParameter(
+            IBioMiniDevice.Parameter(
+                IBioMiniDevice.ParameterType.TIMEOUT,
+                30000 // 30 seconds
             )
-        }
+        )
     }
 
     override fun capture(): CaptureResult? {
-        val latch = CountDownLatch(1)
-        setTemplateType()
-        doSingleCapture(latch)
-        latch.await()
+        return mCurrentDevice?.let {
+            val latch = CountDownLatch(1)
+            setParameters(it)
+            doSingleCapture(latch)
+            latch.await()
 
-        val tmp = mTemplateData
-        return if (tmp?.data != null) {
-            CaptureResult(tmp.data.toHexString(), mFpQuality ?: 0)
-        } else {
-            null
+            val tmp = mTemplateData
+            return if (tmp?.data != null) {
+                CaptureResult(tmp.data.toHexString(), mFpQuality ?: 0)
+            } else {
+                null
+            }
         }
     }
 
