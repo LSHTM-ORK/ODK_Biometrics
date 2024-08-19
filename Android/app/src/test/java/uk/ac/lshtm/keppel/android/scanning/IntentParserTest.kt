2adb2824dfaef07ad3de35da920631054d867add
Callum Stott <callum@seadowg.com>
2024-08-19 16:10:27 +0100

    Add tests for iso template mistakes

--- Android/app/src/test/java/uk/ac/lshtm/keppel/android/scanning/IntentParserTest.kt
+++ Android/app/src/test/java/uk/ac/lshtm/keppel/android/scanning/IntentParserTest.kt
@@ -3,6 +3,7 @@ package uk.ac.lshtm.keppel.android.scanning
 import android.content.Intent
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.containsInAnyOrder
 import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,5 +32,28 @@ class IntentParserTest {
 
         val request = IntentParser.parse(intent)
         assertThat(request.fast, equalTo(false))
+    }
+
+    @Test
+    fun `isoTemplates is empty when list of templates does not start at 1`() {
+        val intent = Intent().also {
+            it.action = External.ACTION_MATCH
+            it.putExtra(External.paramIsoTemplate(2), "blah")
+        }
+
+        val request = IntentParser.parse(intent)
+        assertThat((request as Request.Match).isoTemplates, equalTo(emptyList()))
+    }
+
+    @Test
+    fun `isoTemplates does not parse gaps`() {
+        val intent = Intent().also {
+            it.action = External.ACTION_MATCH
+            it.putExtra(External.paramIsoTemplate(1), "blah1")
+            it.putExtra(External.paramIsoTemplate(3), "blah3")
+        }
+
+        val request = IntentParser.parse(intent)
+        assertThat((request as Request.Match).isoTemplates, containsInAnyOrder("blah1"))
     }
 }
