9e923986fe2da115f18e2d7eb4660bfa14c7bb6e
Tobias McNulty <tobias@caktusgroup.com>
2024-10-24 08:17:07 -0400

    Upgrade SourceAFIS to 3.18.1

--- Android/app/src/main/java/uk/ac/lshtm/keppel/android/matching/SourceAFISMatcher.kt
+++ Android/app/src/main/java/uk/ac/lshtm/keppel/android/matching/SourceAFISMatcher.kt
@@ -1,16 +1,16 @@
 package uk.ac.lshtm.keppel.android.matching
 
-import com.machinezoo.sourceafis.FingerprintCompatibility.convert
+import com.machinezoo.sourceafis.FingerprintCompatibility.importTemplate
+import com.machinezoo.fingerprintio.TemplateFormatException
 import com.machinezoo.sourceafis.FingerprintMatcher
 import uk.ac.lshtm.keppel.core.Matcher
 
 class SourceAFISMatcher : Matcher {
     override fun match(one: ByteArray, two: ByteArray): Double? {
         return try {
-            FingerprintMatcher()
-                .index(convert(one))
-                .match(convert(two))
-        } catch (e: IllegalArgumentException) {
+            FingerprintMatcher(importTemplate(one))
+                .match(importTemplate(two))
+        } catch (e: TemplateFormatException) {
             null
         }
     }
